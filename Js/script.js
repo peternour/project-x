31fd9fd081fe3966ccbfbe7d8385a1624c84425a
peternour <peternour@hotmail.com>
2025-01-28 18:13:52 +0200

    modified peter@home

--- Js/script.js
+++ Js/script.js
@@ -3,8 +3,8 @@ let ageResult = "5465";
 let ageinput = document.querySelector(".date-in").value;
 document.querySelector(".date-lbl").textContent;
 
-document.querySelector(".calc-btn").addEventListener("click", function () {
- ageResult = ageinput;
+document.querySelector(".date-lbl").addEventListener("click", function () {
+ document.querySelector(".date-lbl").textContent = "Peter";
 });
 
 // Parse the input date
