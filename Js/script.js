e692e44255166f5d5842891c7eb8e150f06e1046
peternour <peternour@hotmail.com>
2025-02-09 00:34:02 +0200

    update js file and css and html

--- Js/script.js
+++ Js/script.js
@@ -24,33 +24,16 @@ calcBtn.addEventListener("click", function () {
   let prevMonth = new Date(today.getFullYear(), today.getMonth(), 0).getDate();
   days += prevMonth;
  }
- calcLbl.textContent = `You are ${years} years, ${months} months, and ${days} days old.`;
+
+ if (years < 21) {
+  calcLbl.textContent = `You are UNDER_AGE 21 You are ${years} years, ${months} months, and ${days} days old. And Need to Wait ⌛ ${
+   21 - years
+  } Years To Come Here 😋`;
+ } else if (years > 60) {
+  calcLbl.textContent = `👳‍♂️ You are OVER_AGE 60 You are ${years} years, ${months} months, and ${days} days old And Above 60 By ${
+   years - 60
+  } Year.`;
+ } else {
+  calcLbl.textContent = `✔ You are ${years} years, ${months} months, and ${days} days old.`;
+ }
 });
-
-// Prompt the user to input their birthdate in the format YYYY-MM-DD
-//let birthDateInput = prompt("Enter your birthdate (YYYY-MM-DD):");
-
-// Parse the input date
-//let birthDate = new Date(dateIn);
-//let today = new Date();
-
-// Calculate the difference in years
-//let years = today.getFullYear() - birthDate.getFullYear();
-
-// Adjust the year if the birthday hasn't occurred yet this year
-//let months = today.getMonth() - birthDate.getMonth();
-//if (months < 0 || (months === 0 && today.getDate() < birthDate.getDate())) {
-// years--;
-// months += 12;
-//}
-
-// Calculate the difference in days
-/*let days = today.getDate() - birthDate.getDate();
- if (days < 0) {
-  months--;
-  // Get the number of days in the previous month
-  let prevMonth = new Date(today.getFullYear(), today.getMonth(), 0).getDate();
-  days += prevMonth;
- }*/
-
-// Show the age in an alert
